aab2616ac8313591690faa6465ab7bfe0a3b2612
Scott W Locke <scott.w.locke@gmail.com>
2025-11-23 05:09:15 +0000

    removed old content

--- old.devcontainer/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-
-Import-Module Terminal-Icons
-
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-oh-my-posh init pwsh --config $env:POSH_THEME | Invoke-Expression
-
-# NOTE: You can override the above env var from the devcontainer.json "args" under the "build" key.
